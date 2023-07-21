ccfadde82550790df00ff0fbc6064ac714107a38
Marina Shnyakina <schnyakina@yandex.ru>
2023-07-21 09:40:06 +0300

    Prepare the start code for the new Unscramble app

---
+++ app/src/main/java/com/example/practicingkotlinandandroidstudio/Unscramble/MainClassUnscramble.kt
@@ -0,0 +1,13 @@
+package com.example.practicingkotlinandandroidstudio.Unscramble
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.practicingkotlinandandroidstudio.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity_unscramble)
+    }
+}
