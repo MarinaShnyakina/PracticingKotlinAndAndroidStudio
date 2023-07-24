13bc0b00e93a01c7b7f669f4818f68548422a741
Marina Shnyakina <schnyakina@yandex.ru>
2023-07-24 19:04:31 +0300

    Download the start code for the Cupcake app

---
+++ app/src/main/java/com/example/practicingkotlinandandroidstudio/cupcake/MainClassCupcake.kt
@@ -0,0 +1,10 @@
+package com.example.practicingkotlinandandroidstudio.cupcake
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.practicingkotlinandandroidstudio.R
+
+/**
+ * Activity for cupcake order flow.
+ */
+class MainClassCupcake : AppCompatActivity(R.layout.main_activity_cupcake) {
+}
