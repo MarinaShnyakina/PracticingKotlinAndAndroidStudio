cdbc057192418ef7cbf993e7bd373416c6dd180d
Marina Shnyakina <schnyakina@yandex.ru>
2023-10-26 14:33:04 +0300

    Implement the network layer using the Retrofit library. Parse the JSON response using the Moshi library.

--- app/src/main/java/com/example/practicingkotlinandandroidstudio/marsphotos/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/practicingkotlinandandroidstudio/marsphotos/overview/OverviewViewModel.kt
@@ -3,6 +3,9 @@ package com.example.practicingkotlinandandroidstudio.marsphotos.overview
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.practicingkotlinandandroidstudio.marsphotos.network.MarsApi
+import kotlinx.coroutines.launch
 
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
@@ -26,6 +29,13 @@ class OverviewViewModel : ViewModel() {
      * [MarsPhoto] [List] [LiveData].
      */
     private fun getMarsPhotos() {
-        _status.value = "Set the Mars API status response here!"
+        viewModelScope.launch {
+            try {
+                val listResult = MarsApi.retrofitService.getPhotos()
+                _status.value = "Success: ${listResult.size} Mars photos retrieved"
+            } catch (e: Exception) {
+                _status.value = "Failure: ${e.message}"
+            }
+        }
     }
 }
