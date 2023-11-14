da390dfc58552e3e52d9e231b3b871f1d2c55dd5
Marina Shnyakina <schnyakina@yandex.ru>
2023-11-14 15:26:38 +0300

    Add interface Dao

---
+++ app/src/main/java/com/example/practicingkotlinandandroidstudio/inventory/data/ItemDao.kt
@@ -0,0 +1,16 @@
+package com.example.practicingkotlinandandroidstudio.inventory.data
+
+import androidx.room.*
+
+@Dao
+interface ItemDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(item: Item)
+
+    @Update
+    suspend fun update(item: Item)
+
+    @Delete
+    suspend fun delete(item: Item)
+}
