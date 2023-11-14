d5d93c7e46590a331014339a7a448141ae925e34
Marina Shnyakina <schnyakina@yandex.ru>
2023-11-14 16:10:25 +0300

    Create a RoomDatabase use the Entity and DAO

---
+++ app/src/main/java/com/example/practicingkotlinandandroidstudio/inventory/data/ItemRoomDatabase.kt
@@ -0,0 +1,33 @@
+package com.example.practicingkotlinandandroidstudio.inventory.data
+
+import android.content.Context
+import androidx.room.*
+
+@Database(entities = [Item::class], version = 1, exportSchema = false)
+abstract class ItemRoomDatabase : RoomDatabase() {
+
+    abstract fun itemDao(): ItemDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: ItemRoomDatabase? = null
+
+        fun getDatabase(context: Context): ItemRoomDatabase {
+            // if the INSTANCE is not null, then return it,
+            // if it is, then create the database
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ItemRoomDatabase::class.java,
+                    "item_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+
+                INSTANCE = instance
+                return instance
+            }
+        }
+
+    }
+}
