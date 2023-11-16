cbedbbab6f059a7741c5830ebcd92c1e8883ff04
Marina Shnyakina <schnyakina@yandex.ru>
2023-11-16 18:27:04 +0300

    Add a list adapter to the RecyclerView

---
+++ app/src/main/java/com/example/practicingkotlinandandroidstudio/inventory/ItemListAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.practicingkotlinandandroidstudio.inventory
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.practicingkotlinandandroidstudio.databinding.ItemListItemBinding
+import com.example.practicingkotlinandandroidstudio.inventory.data.Item
+import com.example.practicingkotlinandandroidstudio.inventory.data.getFormattedPrice
+
+class ItemListAdapter(private val onItemClicked: (Item) -> Unit) :
+    ListAdapter<Item, ItemListAdapter.ItemViewHolder>(DiffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        return ItemViewHolder(
+            ItemListItemBinding.inflate(
+                LayoutInflater.from(parent.context)
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val current = getItem(position)
+        holder.itemView.setOnClickListener {
+            onItemClicked(current)
+        }
+        holder.bind(current)
+    }
+
+    class ItemViewHolder(private var binding: ItemListItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+            fun bind(item: Item) {
+                binding.itemName.text = item.itemName
+                binding.itemPrice.text = item.getFormattedPrice()
+                binding.itemQuantity.text = item.quantityInStock.toString()
+            }
+    }
+
+    companion object {
+        private val DiffCallback = object : DiffUtil.ItemCallback<Item>() {
+            override fun areItemsTheSame(oldItem: Item, newItem: Item): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: Item, newItem: Item): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
