8a63cfa30039f2e02a0d203a93545264017f3da6
Marina Shnyakina <schnyakina@yandex.ru>
2023-11-16 19:28:37 +0300

    Update InventoryViewModel and ItemListFragment to display the item details on the screen using the list adapter

--- app/src/main/java/com/example/practicingkotlinandandroidstudio/inventory/ItemListFragment.kt
+++ app/src/main/java/com/example/practicingkotlinandandroidstudio/inventory/ItemListFragment.kt
@@ -5,12 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.practicingkotlinandandroidstudio.R
 import com.example.practicingkotlinandandroidstudio.databinding.ItemListFragmentBinding
+import com.example.practicingkotlinandandroidstudio.inventory.InventoryViewModel
+import com.example.practicingkotlinandandroidstudio.inventory.InventoryViewModelFactory
+import com.example.practicingkotlinandandroidstudio.inventory.ItemListAdapter
 
 class ItemListFragment : Fragment() {
+
+    private val viewModel: InventoryViewModel by activityViewModels {
+        InventoryViewModelFactory(
+            (activity?.application as InventoryApplication).database.itemDao()
+        )
+    }
 
     private var _binding: ItemListFragmentBinding? = null
     private val binding get() = _binding!!
@@ -26,6 +36,15 @@ class ItemListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val adapter = ItemListAdapter {
+        }
+        binding.recyclerView.adapter = adapter
+        viewModel.allItems.observe(this.viewLifecycleOwner) { items ->
+            items.let {
+                adapter.submitList(it)
+            }
+        }
         binding.recyclerView.layoutManager = LinearLayoutManager(this.context)
         binding.floatingActionButton.setOnClickListener {
             val action = ItemListFragmentDirections.actionItemListFragmentToAddItemFragment(
