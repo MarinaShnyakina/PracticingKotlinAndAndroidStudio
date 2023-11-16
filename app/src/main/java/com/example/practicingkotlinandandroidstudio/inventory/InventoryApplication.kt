92871715815cea8e237802fb49981d60255645b3
Marina Shnyakina <schnyakina@yandex.ru>
2023-11-16 12:11:52 +0300

    Create an instance ot the database in the InventoryApplication class

--- app/src/main/java/com/example/practicingkotlinandandroidstudio/inventory/InventoryApplication.kt
+++ app/src/main/java/com/example/practicingkotlinandandroidstudio/inventory/InventoryApplication.kt
@@ -1,7 +1,11 @@
 package com.example.practicingkotlinandandroidstudio.invertory
 
 import android.app.Application
+import com.example.practicingkotlinandandroidstudio.inventory.data.ItemRoomDatabase
 
 class InventoryApplication : Application() {
+    val database: ItemRoomDatabase by lazy {
+        ItemRoomDatabase.getDatabase(this)
+    }
 
 }
